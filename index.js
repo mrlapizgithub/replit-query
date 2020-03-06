8fc16ca35c43faa88ee699b48a7dde47eb81f245
Junhao Zhang <thepuzzlecoderuser@gmail.com>
2020-03-06 08:29:17 -0600

    Fixed error with logging in

--- index.js
+++ index.js
@@ -2,24 +2,9 @@ const talk = require("./lib/talk.js");
 const query = require("./lib/query.js");
 
 const client = () => {
-  const headers = {
-    "Content-Type": "application/json",
-    "Accept": "application/json",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Connection": "keep-alive",
-    "X-Requested-With": "any",
-    "Referrer": "https://repl.it",
-    "Origin": "https://repl.it"
-  };
   return {
-    query,
-    login: (username, password) => {
-      return fetch("https://repl.it/login", {
-        method: "POST",
-        headers,
-        body: JSON.stringify({username, password})
-      }).then(e => e.json()).catch(e => console.error("replit query error:", e))
-    },
+    query: query.query,
+    login: query.login,
     talk
   };
 };
